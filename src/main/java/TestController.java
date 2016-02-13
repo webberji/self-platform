24edfd5ac9e54f0faee217f8aec4b2eb9f045388
jibowei <jibowei@foxmail.com>
2016-02-13 22:59:40 +0800

    添加图片轮播

--- src/main/java/TestController.java
+++ src/main/java/TestController.java
@@ -1,5 +1,10 @@
 /**
  * Created by jibowei on 16/2/2.
  */
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TestController {
 }
---
+++ src/main/webapp/nav/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by jibowei on 16/2/13.
+ */
+
+$(function () {
+    $('#main-frame').attr('src','image-carousel.html');
+});
